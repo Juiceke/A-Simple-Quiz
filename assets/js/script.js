4facac674cb37cf60e8567701aa64f21cc884eb1
Brett herrell <brett_herrell@yahoo.com>
2021-09-04 15:43:11 -0500

    added some example questions to test with

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,62 @@
 var timerEl = document.getElementById('time');
 var startQuiz = document.getElementById('start-quiz');
+var question = document.getElementById('question');
+var paragraph = document.getElementById('paragraph');
 var timeLeft = 60;
+var score = timeLeft;
+var questionArr = [ 
+    {
+    question: "Who invented JavaScript?",
+    answers: {
+      a: "Douglas Crockford",
+      b: "Sheryl Sandberg",
+      c: "Brendan Eich"
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "Which one of these is a JavaScript package manager?",
+    answers: {
+      a: "Node.js",
+      b: "TypeScript",
+      c: "npm"
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "Which tool can you use to ensure code quality?",
+    answers: {
+      a: "Angular",
+      b: "jQuery",
+      c: "RequireJS",
+      d: "ESLint"
+    },
+    correctAnswer: "d"
+  }
+];
+
+var start = function() {
+// replacing content of the page with questions
+question.textContent = 'ayyyy';
+paragraph.remove();
+startQuiz.remove();
+
+
+// adding functionality to the timer
+timerEl.textContent = timeLeft
 
 var time = setInterval(function() {
     
-    timerEl.textContent = timeLeft;
+    timeLeft--;
     if(timeLeft === 0) {
         clearInterval(time);
     }
-    timeLeft--;
+    timerEl.textContent = timeLeft;
     
 },1000)
+    }
     
-    
+startQuiz.onclick = start;
 
 
-startQuiz.onclick = time;
 
